8e9f36ef7f59c39a896abbcab745123be7e10739
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-12-07 19:50:41 +0100

    Revert "finished"
    This reverts commit 2a2a8789e964d03c55d9e7c110274388630a5fd6.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,99 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AltaComponent } from './alta/alta.component';
-import { ListaComponent } from './lista/lista.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AltaComponent, ListaComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'UD38 Ejercicio1';
+  title = 'UD37';
+  
+  expression: string = '';
+  result: string = '';
 
+  appendToExpression(value: string): void {
+    this.expression += value;
+  }
+
+  calculateResult(): void {
+    try {
+      this.result = this.evaluateExpression(this.expression).toString();
+    } catch (error) {
+      this.result = 'Error';
+    }
+  }
+
+  clearExpression(): void {
+    this.expression = '';
+    this.result = '';
+  }
+
+  deleteLast(): void {
+    if (this.expression.length > 0) {
+      this.expression = this.expression.slice(0, -1);
+    }
+  }
+
+  evaluateExpression(expression: string): number {
+    const operators = ['+', '-', '*', '/'];
+    const tokens = expression.split(/([+\-*/])/).filter(token => token.trim() !== '');
+
+    // Perform multiplication and division first
+    for (let i = 0; i < operators.length; i += 2) {
+      for (let j = 1; j < tokens.length; j += 2) {
+        if (tokens[j] === operators[i] || tokens[j] === operators[i + 1]) {
+          const leftOperand = parseFloat(tokens[j - 1]);
+          const rightOperand = parseFloat(tokens[j + 1]);
+
+          if (isNaN(leftOperand) || isNaN(rightOperand)) {
+            throw new Error('Invalid expression');
+          }
+
+          if (tokens[j] === '*') {
+            tokens.splice(j - 1, 3, (leftOperand * rightOperand).toString());
+            j -= 2;
+          } else if (tokens[j] === '/') {
+            if (rightOperand === 0) {
+              throw new Error('Division by zero');
+            }
+            tokens.splice(j - 1, 3, (leftOperand / rightOperand).toString());
+            j -= 2;
+          }
+        }
+      }
+    }
+
+    // Perform addition and subtraction
+    for (let i = 0; i < operators.length; i += 2) {
+      for (let j = 1; j < tokens.length; j += 2) {
+        if (tokens[j] === operators[i] || tokens[j] === operators[i + 1]) {
+          const leftOperand = parseFloat(tokens[j - 1]);
+          const rightOperand = parseFloat(tokens[j + 1]);
+
+          if (isNaN(leftOperand) || isNaN(rightOperand)) {
+            throw new Error('Invalid expression');
+          }
+
+          if (tokens[j] === '+') {
+            tokens.splice(j - 1, 3, (leftOperand + rightOperand).toString());
+            j -= 2;
+          } else if (tokens[j] === '-') {
+            tokens.splice(j - 1, 3, (leftOperand - rightOperand).toString());
+            j -= 2;
+          }
+        }
+      }
+    }
+
+    if (tokens.length !== 1) {
+      throw new Error('Invalid expression');
+    }
+
+    return parseFloat(tokens[0]);
+  }
 }
 
